ae06afcbf7e9445711e5b1d88b5989e71d822765
Jordan Chong <jordanchong23@gmail.com>
2016-10-08 10:53:59 -0700

    Add messages tests for actions and reducers

--- client/src/app/chat/messages/messages_spec.js
+++ client/src/app/chat/messages/messages_spec.js
@@ -1,13 +1,51 @@
 import { renderComponent , expect } from '../../tests/test_helper.js';
 import Messages from './Messages.jsx';
+import MessageReducers from './messages_reducers.js';
+import { FETCH_MESSAGES, POST_MESSAGE, EDIT_MESSAGE } from './messages_actions.js';
+import data from './fixtures/fakedata.js';
 
 describe('Messages' , () => {
-  let component;
+  let component, fakeMessage;
   beforeEach(() => {
    component = renderComponent(Messages);
+   fakeMessage = {
+      id: 99,
+      timestamp: 1475948656962,
+      author: 'Anonymous',
+      content: 'Hello World',
+    };
+  });
+  // Markup
+  describe('Markup', () => {
+    it('has a messages class', () => {
+      expect(component.hasClass('messages')).to.equal(true);
+    });
+  });
+  // Actions
+  // Reducers
+  describe('Reducers', () => {
+    it('handles actions with unknown type', () => {
+      // expect(commentReducer()).to.be.instanceof(Array);
+      expect(MessageReducers(undefined, {})).to.eql([]);
+    });
+    it('handles action of type FETCH_MESSAGES', () => {
+      const action = { type: FETCH_MESSAGES, payload: data };
+      expect(MessageReducers([], action).length).to.eql(data.messages.length);
+    });
+    it('handles action of type POST_MESSAGE', () => {
+      const action = { type: POST_MESSAGE, payload: fakeMessage };
+      expect(MessageReducers([], action)[0]).to.eql(fakeMessage);
+    });
+    it('handles action of type EDIT_MESSAGE', () => {
+      const action = { 
+        type: EDIT_MESSAGE, 
+        payload:  {
+          index: 0,
+          newMessage: fakeMessage
+        }
+      };
+      expect(MessageReducers(data.messages, action)[0]).to.eql(fakeMessage);
+    });
   });
 
-  it('has a messages class', () => {
-    expect(component.hasClass('messages')).to.equal(true);
-  });
 });
